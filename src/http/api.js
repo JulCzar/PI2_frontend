1a2634a32391cc1dcbbaf22d82542dab35d169d6
JulCzar <julio.monteiro@tonolucro.com.br>
2021-12-17 11:50:25 -0300

    added default timeout

--- src/http/api.js
+++ src/http/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  timeout: process.env.REACT_APP_API_TIMEOUT_REQUEST,
+  timeout: process.env.REACT_APP_API_TIMEOUT_REQUEST ?? 5000,
 });
